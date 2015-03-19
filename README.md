e1b744fd3a5bbceb696196ca23545925e1ab7caf
Dávid Halász <skateman@skateman.eu>
2015-03-19 11:15:09 +0100

    Initial commit!

---
+++ README.md
@@ -0,0 +1,42 @@
+# hwping
+IRC bot for HW pinging with the Dream Cheeky Thunder missile launcher.
+
+## Installation
+The bot was tested on ruby 2.0.0-p643, but it should work with other versions.
+```bash
+cp config_sample.yml config.yml
+bundle install
+```
+
+### Configuration
+The bot uses `config.yml` as a configuration file, which is automatically saved upon exit. It has some default settings, so it will even run without it. For all available settings see the `config_sample.yml` file.
+
+## Usage
+```bash
+bundle exec ruby hwping.rb
+```
+It's important that the bot will react only when an authorized user (specified in the configuration file) sends him a message!
+
+### Channel message commands:
+- `hwping <nick>` - fire a rocket at &lt;nick&gt; when he has been set as a target and is present in the channel's user list
+- `hwping joke` - calls the api from icndb.com to tell a random fact about &lt;botnick&gt;
+
+### Private message commands:
+- `help` - displays a help text
+- `fire` - fires a rocket
+- `reset` - resets the launcher into the default position (bottom-left)
+- `position` - returns the actual position of the rocket launcher
+- `<direction> <ms>` - rotates the launcher into the given direction (up, down, left, right) for the given milliseconds
+- `target list` - displays the list of the available targets
+- `target get <nick>` - displays the coordinates of &lt;nick&gt;
+- `target del <nick>` - deletes the coordinates of &lt;nick&gt;
+- `target set <nick>` - sets the coordinates of &lt;nick&gt; to the actual position
+- `target set <nick> <X> <Y>` - sets the coordinates of &lt;nick&gt; to right(X), up(Y)
+
+## TODO
+- Listen to a nick other than `hwping`
+- High-precision timer for more precise positioning
+- Use event-based solution instead of threads
+
+## Credits
+The launcher code in `lib/launcher.rb` was inspired by [robhurring/thunder](https://github.com/robhurring/thunder).
---
+++ lib/launcher.rb
@@ -0,0 +1,124 @@
+# Inspired by https://github.com/robhurring/thunder/blob/master/lib/launcher.rb
+require 'libusb'
+
+class Launcher
+
+  DeviceNotFoundError = Class.new(IOError)
+
+  DEVICE = {
+    :vendor_id  => 0x2123,
+    :product_id => 0x1010
+  }
+
+  COMMANDS = {
+    :down  => 0x01,
+    :up    => 0x02,
+    :left  => 0x04,
+    :right => 0x08,
+    :fire  => 0x10,
+    :stop  => 0x20,
+    :on    => 0x01,
+    :off   => 0x00
+  }
+
+  TARGETS = {
+    :launcher => 0x02,
+    :led      => 0x03
+  }
+
+  REQUEST_TYPE  = 0x21
+  REQUEST       = 0x09
+
+  def self.connect
+    usb = LIBUSB::Context.new
+    launcher = usb.devices(
+      idVendor: DEVICE[:vendor_id],
+      idProduct: DEVICE[:product_id]
+    ).first
+
+    raise DeviceNotFoundError, 'Launcher was not found.' if launcher.nil?
+    new(launcher)
+  end
+
+  def initialize(device)
+    @device = device
+    @handle = @device.open
+    @x = 0
+    @y = 0
+  end
+
+  def point_and_fire(arr)
+    led(:on)
+    reset
+    right arr[0]
+    up arr[1]
+    fire
+    led(:off)
+  end
+
+  def position
+    [@x, @y]
+  end
+
+  def led(status)
+    send! :led, status
+  end
+
+  def fire
+    send! :launcher, :fire
+    Kernel.sleep 4.5
+  end
+
+  def reset
+    down(1000)
+    left(6000)
+    @x = 0
+    @y = 0
+  end
+
+  def move(direction, duration)
+    send! :launcher, direction
+    Kernel.sleep duration.to_f / 1000
+    send! :launcher, :stop
+    update_pos direction, duration
+  end
+
+  [:up, :down, :left, :right].each do |direction|
+    define_method direction do |duration|
+      move direction, duration
+    end
+  end
+
+
+private
+  def send!(target, command)
+    payload = build_payload(target, COMMANDS[command.to_sym])
+
+    @handle.control_transfer(
+      bmRequestType: REQUEST_TYPE,
+      bRequest: REQUEST,
+      wValue: 0,
+      wIndex: 0,
+      dataOut: payload,
+      timeout: 0
+    )
+  end
+
+  def build_payload(target, command)
+    [TARGETS[target], command, 0, 0, 0, 0, 0, 0].pack('CCCCCCCC')
+  end
+
+  def update_pos(direction, value)
+    value = value.to_i
+    case direction
+    when :up
+      @y = [1000, @y + value].min
+    when :down
+      @y = [0, @y - value].max
+    when :right
+      @x = [6000, @x + value].min
+    when :left
+      @x = [0, @x - value].max
+    end
+  end
+end
